2a6b57fd00cf5f614b59f0161713aa06eeb41da1
a <a@a.a>
2025-06-03 11:33:13 +0200

    .

--- notepad/sw.js
+++ notepad/sw.js
@@ -1,5 +1,6 @@
-const ver = 'v.js04';
-const cacheName = `n-cache-${ver}`;
+const ver = 'v.js03';
+const cacheName = `np-cache-${ver}`;
+
 const urlsToCache = [
   './',
   './index.html',
@@ -8,13 +9,29 @@ const urlsToCache = [
   './manifest.json',
   './192x192.png'
 ];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
-self.addEventListener('message', (event) => event.data.action === 'skipWaiting' && self.skipWaiting());
+
+self.addEventListener('message', (event) => {
+  event.data.action === 'skipWaiting' && self.skipWaiting();
+  event.data.type === 'getSharedData' && event.source.postMessage({ sharedData: 'example data' });
+});
+
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
+
+  if (event.request.method === 'POST') {
+    setTimeout(()=>{
+    const chan = new BroadcastChannel("foo");
+    chan.postMessage(event.request);
+    chan.close();
+    event.respondWith(handleShare(event.request));
+    },4000);
+  }
+
   if (url.origin === self.location.origin) {
     event.respondWith(
       caches.match(event.request).then((cachedResponse) => {
@@ -49,3 +66,11 @@ self.addEventListener('activate', (event) => {
   );
   self.clients.claim();
 });
+
+async function handleShare(request) {
+  const formData = await request.formData();
+  const text = formData.get('text') || '';
+  const title = formData.get('title') || '';
+  const url = formData.get('url') || '';
+  return Response.redirect(`./index.html?shared=true&title=${encodeURIComponent(title)}&text=${encodeURIComponent(text)}&url=${encodeURIComponent(url)}`, 303);
+}
