b8eda2920952eef60949e2d8559dab5808c3df1c
azatom <azatom@gmail.com>
2025-09-24 06:57:27 +0200

    refactor

--- stopwatch/sw.js
+++ stopwatch/sw.js
@@ -1,4 +1,5 @@
-const cacheName = `cache-v1.2`;
+const ver = "1.3";
+const cacheName = `cache-v${ver}`;
 const urlsToCache = [
     '.',
     './manifest.json',
@@ -6,41 +7,58 @@ const urlsToCache = [
     './favicon.ico',
 ];
 const urlAlias = { "./": ".", "./index.html": "." };
-self.addEventListener('install', (event) => event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(urlsToCache))));
-self.addEventListener('fetch', (event) => {
-    const url = new URL(event.request.url);
-    if (url.origin === self.location.origin) {
-        const ali = urlAlias[url.pathname.replace(/^\//, './')];
-        const cacheRequest = ali
-            ? new Request(ali, { method: event.request.method, headers: event.request.headers })
-            : event.request;
-        event.respondWith(
-            caches.match(cacheRequest).then(async (cachedResponse) =>
-                cachedResponse ||
-                fetch(event.request).then(async (networkResponse) => {
-                    const cache = await caches.open(cacheName);
-                    cache.put(event.request, networkResponse.clone());
-                    return networkResponse;
-                }).catch(async () => event.request.mode === 'navigate'
-                    ? new Response("Sorry...Need...Net...\n" + event.request.url, { headers: { 'Content-Type': 'text/plain' } })
-                    : caches.match(cacheRequest)
-                )
-            )
-        );
-    } else {
-        event.respondWith(
-            fetch(event.request).catch(() => caches.match(event.request))
-        );
-    }
-});
+self.addEventListener('install', (event) => event.waitUntil(
+    caches.open(cacheName)
+        .then(cache => cache.addAll(urlsToCache))
+        .then(() => self.skipWaiting())
+));
 self.addEventListener('activate', (event) => {
     const cacheWhitelist = [cacheName];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => cacheWhitelist.includes(cacheName) || caches.delete(cacheName))
-            );
-        })
-    );
-    self.clients.claim();
+    event.waitUntil(Promise.all([
+        caches.keys()
+            .then((cacheNames) => Promise.all(
+                cacheNames
+                    .filter((name) => !cacheWhitelist.includes(name))
+                    .map((name) => caches.delete(name))
+            )),
+        self.clients.claim()
+    ]));
 });
+self.addEventListener('fetch', (event) => {
+    const request = getCacheRequest(event.request);
+    event.respondWith(
+        caches.match(request)
+            .then((cachedResponse) => cachedResponse || fetchAndCache(request, event.request))
+            .catch(() => handleOffline(request, event.request))
+    );
+});
+function getCacheRequest(request) {
+    const url = new URL(request.url);
+    if (url.origin !== self.location.origin) return request;
+    const aliasedUrl = urlAlias[url.pathname.replace(/^\//, './')];
+    return aliasedUrl
+        ? new Request(aliasedUrl, { method: request.method, headers: request.headers })
+        : request;
+}
+async function fetchAndCache(cacheRequest, originalRequest) {
+    const networkResponse = await fetch(originalRequest);
+    if (networkResponse.ok) {
+        const cache = await caches.open(cacheName);
+        await cache.put(originalRequest, networkResponse.clone());
+    }
+    return networkResponse;
+}
+function handleOffline(cacheRequest, originalRequest) {
+    return originalRequest.mode === 'navigate'
+        ? new Response(`Sorry...Need...Net... ${originalRequest.url}`, { headers: { 'Content-Type': 'text/plain' } })
+        : caches.match(cacheRequest);
+}
+
+self.addEventListener('message', (event) => event.data.action === 'skipWaiting' && self.skipWaiting());
+new BroadcastChannel("main").onmessage = ({ data }) => {
+    if (data.q === "ver") {
+        const chan = new BroadcastChannel(data.respondAt);
+        chan.postMessage(ver);
+        chan.close();
+    }
+};
