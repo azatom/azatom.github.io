7bd28bb5017462d9bcd22b6ad6ed28c790b7b1f9
azatom <azatom@gmail.com>
2025-12-08 02:32:32 +0100

    terser

--- lsystem/build.js
+++ lsystem/build.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { build } from 'esbuild';
 import { readFile, writeFile } from 'node:fs/promises';
+import { minify } from 'terser';
 
 const srcHtml = 'src/editor.html';
 const outSrcSvg = 'src/lsystem.svg';
@@ -9,48 +10,64 @@ const outHtml = 'index.html';
 const srcs = [{
   entryPoints: ['src/editor.js'],
   format: 'iife',
+  // minify: true,
 }, {
   entryPoints: ['src/lsystem.svg.onload.js'],
   format: 'esm',
+  // minify: true,
 }, {
   entryPoints: ['src/editor.css'],
+  // minify: true,
 }]
 
 const failOnWarning = async (options) => {
-  const result = await build({
+  options = {
     write: false,
     bundle: true,
     minify: true,
     ...options
-  });
+  };
+  const result = await build(options);
   [...result.warnings, ...result.errors]
     .map(e => console.error(e.text))
     .some(() => process.exit(1));
-  return result.outputFiles[0].text.trim();
+  return options?.minify
+    ? result.outputFiles[0].text.trim()
+    : result.outputFiles[0].text;
+};
+const terseropts = {
+  compress: {
+    defaults: true,
+    dead_code: true,
+    booleans: true,
+    conditionals: true,
+  },
+  format: { quote_style: 1, },
+  mangle: true,
 };
 
 const [js, onload, css] = await Promise.all(srcs.map(failOnWarning));
-
-// TODO: use terser(?): unfavor '&&' and '"'
-const escaped = onload
-  .replace(/"([^"\\]|\\.)*"/g, m => `'${m.slice(1, -1).replace(/'/g, "\\'")}'`)
-  .replace(/&/g, '&amp;')
-  .replace(/</g, '&lt;')
-  .replace(/"/g, '&quot;')
-  .replace(/\n/g, ' ');
-
-const svg = `<svg onload="${escaped}" xmlns="http://www.w3.org/2000/svg"></svg>`;
+const [js2, onload2] = await Promise.all([js, onload].map(a => minify(a, terseropts)));
 
 const html = (await readFile(srcHtml, 'utf8'))
-  .replace(/<script[^>]*src="editor\.js"[^>]*><\/script>/, `<script>${js}</script>`)
+  .replace(/<script[^>]*src="editor\.js"[^>]*><\/script>/, `<script>${js2.code}</script>`)
   .replace(/<link[^>]*href="editor\.css"[^>]*>/, `<style>${css}</style>`);
+
+const onload3 = onload2.code
+  .replace(/&/g, '&amp;')
+  .replace(/</g, '&lt;')
+  .replace(/"/g, '&quot;');
+
+if (/\n|\r/.test(onload3)) throw new Error('newline');
+
+const svg = `<svg onload="${onload3}" xmlns="http://www.w3.org/2000/svg"></svg>`;
 
 const writeIfChanged = async (filename, data) => {
   let buf = Buffer.from(data), old, res = '-';
   try { old = await readFile(filename); }
   catch (e) { if (e.code !== 'ENOENT') throw e; }
   if (!old || buf.compare(old)) { await writeFile(filename, buf); res = '+'; }
-  console.log(`${res} ${filename}`);
+  console.log(`${res} ${filename} ${buf.length}`);
 };
 
 [
